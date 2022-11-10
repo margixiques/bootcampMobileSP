fc4301a1ac9d27cb7a03987febb94d877d7bdcf4
margixiques <margixiques@gmail.com>
2022-11-09 21:48:03 -0500

    First point completed

--- Challenge1.playground/Contents.swift
+++ Challenge1.playground/Contents.swift
@@ -1,1 +1,37 @@
+/// Challenge 1
+/// - 1. Random String Number Generator
 
+struct StringGenerator {
+    
+    func build(by type: String) -> String {
+        var result = ""
+        
+        switch type {
+        case "TipoA":
+            result = "54"
+        case "TipoB":
+            result = "08"
+        default:
+            return "Opción no válida"
+        }
+        
+        for _ in 0..<8 {
+            let number = Int.random(in: 0...9)
+            result += "\(number)"
+        }
+        return result
+    }
+    
+}
+
+let stringGenerator = StringGenerator()
+print(
+    "La cadena de texto para TipoA es:",
+    stringGenerator.build(by: "TipoA")
+)
+print(
+    "La cadena de texto para TipoB es:",
+    stringGenerator.build(by: "TipoB")
+)
+
+print(stringGenerator.build(by: "TipoC"))
